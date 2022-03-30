b9daefb17323f53139e91ba55d960547c869b94a
inayaka <ina4@wp.eu>
2022-03-30 20:31:21 +0200

    bb

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+console.log("Hello JavaScript!")
+
 const z = 5;
 let x = 7;
 
-console.log(x+z)
+//zmienną let wstawiamy poniżej czyli x = 2; - to 
+//bierze do sumowania teę drugą wartość czyli suma jest 7 nie 12
+x = 2;
+
+// z = 1 - NIE MOŻNA NADPISAĆ STAŁEJ!!!!
+
+console.log(x+z);
+
+// STRING ciąg znaków - zawsze w cudzysłowiu
+
+const string = "asd"
+
+
+const project = {
+    name : "Projekt 1",
+    date : "24.11.2018"
+}
+
+//console.log("Projekt o nazwie " + project.name
+//+ " został stworzony " + project.date)
+
+//console.log(project.name)
+//console.log(project.date)
+
+
+//TABLICE (ARRAYS)
+
+// const fibArray
 
 
 
-
-console.log("Hello JavaScript!")
